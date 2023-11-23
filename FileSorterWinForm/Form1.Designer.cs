2817ce7253827950a01fd894bc1b7f2b5ec0cbc2
Lucas Botelho <Lucas.Botelho@merkle.com>
2023-11-23 11:07:24 +0000

    base code for comparing two images

--- FileSorterWinForm/Form1.Designer.cs
+++ FileSorterWinForm/Form1.Designer.cs
@@ -44,13 +44,15 @@
             this.label2 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.progressBar_label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // sourcePath_button
             // 
-            this.sourcePath_button.Location = new System.Drawing.Point(713, 11);
+            this.sourcePath_button.Location = new System.Drawing.Point(951, 14);
+            this.sourcePath_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sourcePath_button.Name = "sourcePath_button";
-            this.sourcePath_button.Size = new System.Drawing.Size(75, 23);
+            this.sourcePath_button.Size = new System.Drawing.Size(100, 28);
             this.sourcePath_button.TabIndex = 0;
             this.sourcePath_button.Text = "...";
             this.sourcePath_button.UseVisualStyleBackColor = true;
@@ -58,42 +60,47 @@
             // 
             // sourcePath_textBox
             // 
-            this.sourcePath_textBox.Location = new System.Drawing.Point(144, 13);
+            this.sourcePath_textBox.Location = new System.Drawing.Point(192, 16);
+            this.sourcePath_textBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sourcePath_textBox.Name = "sourcePath_textBox";
-            this.sourcePath_textBox.Size = new System.Drawing.Size(554, 20);
+            this.sourcePath_textBox.Size = new System.Drawing.Size(737, 22);
             this.sourcePath_textBox.TabIndex = 1;
             this.sourcePath_textBox.Leave += new System.EventHandler(this.sourcePath_textBox_Leave);
             // 
             // sourcePath_label
             // 
             this.sourcePath_label.AutoSize = true;
-            this.sourcePath_label.Location = new System.Drawing.Point(12, 16);
+            this.sourcePath_label.Location = new System.Drawing.Point(16, 20);
+            this.sourcePath_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.sourcePath_label.Name = "sourcePath_label";
-            this.sourcePath_label.Size = new System.Drawing.Size(107, 13);
+            this.sourcePath_label.Size = new System.Drawing.Size(130, 16);
             this.sourcePath_label.TabIndex = 2;
             this.sourcePath_label.Text = "Indicate source path:";
             // 
             // destionationPath_label
             // 
             this.destionationPath_label.AutoSize = true;
-            this.destionationPath_label.Location = new System.Drawing.Point(12, 57);
+            this.destionationPath_label.Location = new System.Drawing.Point(16, 70);
+            this.destionationPath_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.destionationPath_label.Name = "destionationPath_label";
-            this.destionationPath_label.Size = new System.Drawing.Size(126, 13);
+            this.destionationPath_label.Size = new System.Drawing.Size(154, 16);
             this.destionationPath_label.TabIndex = 5;
             this.destionationPath_label.Text = "Indicate destination path:";
             // 
             // destinationPath_textBox
             // 
-            this.destinationPath_textBox.Location = new System.Drawing.Point(144, 54);
+            this.destinationPath_textBox.Location = new System.Drawing.Point(192, 66);
+            this.destinationPath_textBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.destinationPath_textBox.Name = "destinationPath_textBox";
-            this.destinationPath_textBox.Size = new System.Drawing.Size(554, 20);
+            this.destinationPath_textBox.Size = new System.Drawing.Size(737, 22);
             this.destinationPath_textBox.TabIndex = 4;
             // 
             // destionationPath_button
             // 
-            this.destionationPath_button.Location = new System.Drawing.Point(713, 52);
+            this.destionationPath_button.Location = new System.Drawing.Point(951, 64);
+            this.destionationPath_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.destionationPath_button.Name = "destionationPath_button";
-            this.destionationPath_button.Size = new System.Drawing.Size(75, 23);
+            this.destionationPath_button.Size = new System.Drawing.Size(100, 28);
             this.destionationPath_button.TabIndex = 3;
             this.destionationPath_button.Text = "...";
             this.destionationPath_button.UseVisualStyleBackColor = true;
@@ -104,36 +111,40 @@
             this.fileType_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.fileType_comboBox.Enabled = false;
             this.fileType_comboBox.FormattingEnabled = true;
-            this.fileType_comboBox.Location = new System.Drawing.Point(144, 95);
+            this.fileType_comboBox.Location = new System.Drawing.Point(192, 117);
+            this.fileType_comboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.fileType_comboBox.Name = "fileType_comboBox";
-            this.fileType_comboBox.Size = new System.Drawing.Size(68, 21);
+            this.fileType_comboBox.Size = new System.Drawing.Size(89, 24);
             this.fileType_comboBox.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 98);
+            this.label1.Location = new System.Drawing.Point(16, 121);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.Size = new System.Drawing.Size(106, 16);
             this.label1.TabIndex = 7;
             this.label1.Text = "Indicate file type:";
             // 
             // result_richTextBox
             // 
             this.result_richTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.result_richTextBox.Location = new System.Drawing.Point(15, 127);
+            this.result_richTextBox.Location = new System.Drawing.Point(20, 156);
+            this.result_richTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.result_richTextBox.Name = "result_richTextBox";
             this.result_richTextBox.ReadOnly = true;
             this.result_richTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.result_richTextBox.Size = new System.Drawing.Size(773, 311);
+            this.result_richTextBox.Size = new System.Drawing.Size(1029, 382);
             this.result_richTextBox.TabIndex = 8;
             this.result_richTextBox.Text = "";
             // 
             // runApp_button
             // 
-            this.runApp_button.Location = new System.Drawing.Point(713, 93);
+            this.runApp_button.Location = new System.Drawing.Point(951, 114);
+            this.runApp_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.runApp_button.Name = "runApp_button";
-            this.runApp_button.Size = new System.Drawing.Size(75, 28);
+            this.runApp_button.Size = new System.Drawing.Size(100, 34);
             this.runApp_button.TabIndex = 9;
             this.runApp_button.Text = "Run";
             this.runApp_button.UseVisualStyleBackColor = true;
@@ -146,26 +157,28 @@
             this.sortingAction_comboBox.Items.AddRange(new object[] {
             "Move",
             "Copy"});
-            this.sortingAction_comboBox.Location = new System.Drawing.Point(316, 95);
+            this.sortingAction_comboBox.Location = new System.Drawing.Point(421, 117);
+            this.sortingAction_comboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sortingAction_comboBox.Name = "sortingAction_comboBox";
-            this.sortingAction_comboBox.Size = new System.Drawing.Size(121, 21);
+            this.sortingAction_comboBox.Size = new System.Drawing.Size(160, 24);
             this.sortingAction_comboBox.TabIndex = 10;
-            this.sortingAction_comboBox.SelectedIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(234, 98);
+            this.label2.Location = new System.Drawing.Point(312, 121);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.Size = new System.Drawing.Size(92, 16);
             this.label2.TabIndex = 11;
             this.label2.Text = "Sorting Action:";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(452, 93);
+            this.progressBar1.Location = new System.Drawing.Point(603, 114);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(246, 23);
+            this.progressBar1.Size = new System.Drawing.Size(328, 28);
             this.progressBar1.TabIndex = 12;
             this.progressBar1.Visible = false;
             // 
@@ -173,16 +186,29 @@
             // 
             this.progressBar_label.AutoSize = true;
             this.progressBar_label.BackColor = System.Drawing.Color.Transparent;
-            this.progressBar_label.Location = new System.Drawing.Point(569, 98);
+            this.progressBar_label.Location = new System.Drawing.Point(759, 121);
+            this.progressBar_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.progressBar_label.Name = "progressBar_label";
-            this.progressBar_label.Size = new System.Drawing.Size(0, 13);
+            this.progressBar_label.Size = new System.Drawing.Size(0, 16);
             this.progressBar_label.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 90);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.progressBar_label);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.sortingAction_comboBox);
@@ -197,6 +223,7 @@
             this.Controls.Add(this.sourcePath_textBox);
             this.Controls.Add(this.sourcePath_button);
             this.Controls.Add(this.progressBar1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "File Sorter";
@@ -224,6 +251,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label progressBar_label;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- FileSorterWinForm/Form1.cs
+++ FileSorterWinForm/Form1.cs
@@ -14,6 +14,7 @@ using FileSorterWinForm.Services.Interfaces;
 using FileSorterWinForm.Exceptions;
 using FileSorterWinForm.Repositories.Implementations;
 using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 
 namespace FileSorterWinForm
 {
@@ -139,6 +140,63 @@ namespace FileSorterWinForm
 
             formManagerRepository.WriteFileNameAndExtensionOnTextBox(result_richTextBox, allFiles);
             formManagerRepository.FillComboBoxWithFileExtensions(fileType_comboBox, fileSettingsRepository.GetFilesExtensionsTypes(allFiles.ToList()));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var filesToBeMoved = Directory.GetFiles(sourcePath_textBox.Text,
+                                           $"*.{fileType_comboBox.Items[fileType_comboBox.SelectedIndex].ToString().Trim('.')}",
+                                           SearchOption.AllDirectories);
+
+            var filesToComparedWith = (string[])filesToBeMoved.Clone();
+
+            foreach (var file in filesToBeMoved)
+            {
+
+                if (Path.GetExtension(file).ToLower().Equals("png") || Path.GetExtension(file).ToLower().Equals(".png"))
+                {
+                    foreach (var file1 in filesToBeMoved)
+                    {
+                        if (Path.GetExtension(file1).ToLower().Equals("png") || Path.GetExtension(file1).ToLower().Equals(".png"))
+                        {
+                            string img1_ref, img2_ref;
+                            var img1 = new Bitmap(file);
+                            var img2 = new Bitmap(file1);
+                            int count1 = 0, count2 = 0;
+                            bool flag = true;
+
+                            if (img1.Width == img2.Width && img1.Height == img2.Height)
+                            {
+                                for (int i = 0; i < img1.Width; i++)
+                                {
+                                    for (int j = 0; j < img1.Height; j++)
+                                    {
+                                        img1_ref = img1.GetPixel(i, j).ToString();
+                                        img2_ref = img2.GetPixel(i, j).ToString();
+                                        if (img1_ref != img2_ref)
+                                        {
+                                            count2++;
+                                            flag = false;
+                                            break;
+                                        }
+                                        count1++;
+                                    }
+                                }
+                                if (flag == false)
+                                    MessageBox.Show("Sorry, Images are not same , " + count2 + " wrong pixels found");
+                                else
+                                    MessageBox.Show(" Images are same , " + count1 + " same pixels found and " + count2 + " wrong pixels found");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Sorry, Images are not same");
+                            }
+                        }
+                    }
+
+                }
+            }
+
         }
     }
 }
